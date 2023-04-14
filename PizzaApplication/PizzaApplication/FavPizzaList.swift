77707c04ec0d88870a6f2bd5558484bc416477b3
Gayan21 <68651319+Gayan21@users.noreply.github.com>
2023-04-15 01:43:01 +0530

    built using swiftui

---
+++ PizzaApplication/PizzaApplication/FavPizzaList.swift
@@ -0,0 +1,36 @@
+//
+//  FavPizzaList.swift
+//  PizzaApplication
+//
+//  Created by Ranul Akmeemana on 2023-03-01.
+//
+
+import SwiftUI
+
+struct FavPizzaList: View {
+    @ObservedObject var viewModel :  PizzaViewModel
+    var body: some View {
+        NavigationStack {
+            List {
+                ForEach(viewModel.filterFavouritePizza()) { pizza in
+                    HStack{
+                        Image("\(pizza.imageName ?? "")")
+                            .resizable()
+                            .frame(width: 100, height: 100)
+                        Text("\(pizza.name ?? "")")
+                    }
+                }
+                .onDelete(perform: viewModel.deletePizza)
+            }
+            .listStyle(.plain)
+            .navigationTitle("Favourite Pizzas")
+            .navigationBarTitleDisplayMode(.large)
+        }
+    }
+}
+
+struct FavPizzaList_Previews: PreviewProvider {
+    static var previews: some View {
+        FavPizzaList(viewModel: PizzaViewModel())
+    }
+}
